12b6c09571765c496ff1dc080dfb4c550117b61c
Adam McMaster <adam@zooniverse.org>
2017-05-25 16:16:52 +0100

    Stub out project structure

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hermescli',
+    version='0.1',
+    url='https://github.com/zooniverse/hermes',
+    author='Adam McMaster',
+    author_email='adam@zooniverse.org',
+    description=(
+        'A CLI for deploying stacks to Docker Swarm on AWS'
+    ),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'docker',
+        'paramiko',
+        'boto',
+    ],
+    entry_points='''
+        [console_scripts]
+        hermes=hermes.scripts.hermes:cli
+    ''',
+)
