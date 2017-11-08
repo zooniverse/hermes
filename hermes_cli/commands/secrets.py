026fbc2668b739e019b9bcb5755b80e2f3bedf18
Adam McMaster <adam@zooniverse.org>
2017-11-08 14:36:24 +0000

    Backup secrets to S3 by default

--- hermes_cli/commands/secrets.py
+++ hermes_cli/commands/secrets.py
@@ -1,3 +1,6 @@
+import sys
+
+import boto3
 import click
 
 from hermes_cli.manager import Manager
@@ -10,16 +13,39 @@ def secrets():
 
 
 @secrets.command()
+@click.pass_context
 @click.argument('swarm-name')
 @click.argument('secret-name')
 @click.argument('secret-file', type=click.File('rb'))
-def create(swarm_name, secret_name, secret_file):
-    with Manager.find(swarm_name) as manager:
-        manager.docker.secrets.create(
-            name=secret_name,
-            data=secret_file.read(),
-        )
+@click.option('-n', '--no-backup', is_flag=True)
+def create(ctx, swarm_name, secret_name, secret_file, no_backup):
+    if not no_backup:
+        config_bucket = ctx.parent.parent.config.get('s3_config_bucket')
+        if not config_bucket:
+            click.echo(
+                'No s3_config_bucket configured! Please run hermes configure',
+                err=True,
+            )
+            sys.exit(1)
 
+    secret_data = secret_file.read()
+
+    with Manager.find(swarm_name) as manager:
+        if not no_backup:
+            boto3.resource('s3').Object(
+                config_bucket,
+                'swarms/{}/secrets/{}'.format(swarm_name, secret_name),
+            ).put(
+                ServerSideEncryption='aws:kms',
+                ACL='private',
+                Body=secret_data,
+            )
+        click.echo(
+            manager.docker.secrets.create(
+                name=secret_name,
+                data=secret_data,
+            )
+        )
 
 @secrets.command()
 @click.argument('swarm-name')
